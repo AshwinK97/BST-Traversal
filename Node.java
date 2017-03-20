92b298e07faf3fc8f217f62db4fbe4d0a4f9e768
Ashwin Kamalakannan <ashwin.kamalakannan@uoit.net>
2017-03-20 14:49:16 -0400

    added preorder and postorder traversal

--- Node.java
+++ Node.java
@@ -1,19 +1,10 @@
-/*
-* Binary tree node
-  Find a way to load tree nodes using the different types of traversal as input {1, 2, 3, 4}
-  Then output the other 2 traversals
-*/
-
 public class Node {
 	Node left, right;
 	int data;
 
 	public Node(int data) {
 		this.data = data;
-
 	}
-
-	/* inserts value into tree */
 
 	public void insert(int value) {
 		if (value <= data) { // if insert value is less than current node
@@ -22,7 +13,7 @@ public class Node {
 			else
 				left.insert(value); // if not, recall function for left child node
 		} else { // if insert value is greater than current node
-			if (right == null) // if current noded, right child is null
+			if (right == null) // if current nodes, right child is null
 				right = new Node(value); // create right child and set value
 			else
 				right.insert(value); // if not, recall function for right child node
@@ -47,21 +38,28 @@ public class Node {
 		}
 	}
 
-	/* Print the tree in order from left to right, aka ascending order */
-
 	public void printInOrder() {
 		if (left != null)
 			left.printInOrder();
-		System.out.println(data);
+		System.out.print(data + " ");
 		if (right != null)
 			right.printInOrder();
 	}
-	
+
 	public void printPreOrder() {
-		// TODO lul
+		System.out.print(data + " ");
+		if (left != null)
+			left.printPreOrder();
+		if (right != null)
+			right.printPreOrder();
 	}
-	
+
 	public void printPostOrder() {
-		// TODO this shit
+		if (left != null)
+			left.printPostOrder();
+		if (right != null)
+			right.printPostOrder();
+		System.out.print(data + " ");
 	}
 }
+
