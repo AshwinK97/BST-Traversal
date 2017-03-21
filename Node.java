1031f265e8f1f85b9f7bc356cde05556f4c6ee40
Ashwin Kamalakannan <ashwin.kamalakannan@uoit.net>
2017-03-21 12:57:32 -0400

    added all documentation

--- Node.java
+++ Node.java
@@ -1,11 +1,32 @@
-public class Node {
-	Node left, right;
-	int data;
+/**
+ * @authors:
+ * 		Darron Singh
+ * 		Jasindan Rasalingam
+ * 		Kaushal Patel
+ * 		Ashwin Kamalakannan
+ * 
+ * - Binary tree node class
+ * - Any single instance of a Node an be used to create a whole tree
+ * - contains methods:
+ * 		- recursive insert(int) O(log n) time
+ * 		- recursive contains(int) O(log n) time
+ *  		- printPreOrder(), printInOrder() and printPostOrder() all O(n) time
+ */
 
+public class Node {
+	Node left, right; // left and right child nodes
+	int data; // numerical data for the node
+
+	/*
+	 * constructor takes data for node and initializes it
+	 */
 	public Node(int data) {
 		this.data = data;
 	}
 
+	/*
+	 * takes data and traverses tree to insert at correct position
+	 */
 	public void insert(int value) {
 		if (value <= data) { // if insert value is less than current node
 			if (left == null) // if current node, left child is null
@@ -19,9 +40,10 @@ public class Node {
 				right.insert(value); // if not, recall function for right child node
 		}
 	}
-
-	/* Returns true if tree contains value */
-
+	
+	/*
+	 * finds given data in tree and returns true if it exists, false otherwise
+	 */
 	public boolean contains(int value) {
 		if (value == data) // if value = current node's data, return true
 			return true;
@@ -37,29 +59,38 @@ public class Node {
 				return right.contains(value);
 		}
 	}
-
-	public void printInOrder() {
-		if (left != null)
-			left.printInOrder();
-		System.out.print(data + " ");
-		if (right != null)
-			right.printInOrder();
-	}
-
+	
+	/*
+	 * prints node data from top to bottom, left to right
+	 */
 	public void printPreOrder() {
-		System.out.print(data + " ");
-		if (left != null)
-			left.printPreOrder();
-		if (right != null)
-			right.printPreOrder();
+		System.out.print(data + " "); // print current node
+		if (left != null) // check if left is not empty
+			left.printPreOrder(); // recursively print left
+		if (right != null) // check if right is not empty
+			right.printPreOrder(); // recursively print right
 	}
-
+	
+	/*
+	 * prints node data in ascending order from left to right
+	 */ 
+	public void printInOrder() {
+		if (left != null) // check if left is not empty
+			left.printInOrder(); // recursively print left
+		System.out.print(data + " "); // print current node
+		if (right != null) // check if right is not empty
+			right.printInOrder(); // recursively print right
+	}
+	
+	/*
+	 * prints node data from bottom to top, left to right
+	 */
 	public void printPostOrder() {
-		if (left != null)
-			left.printPostOrder();
-		if (right != null)
-			right.printPostOrder();
-		System.out.print(data + " ");
+		if (left != null) // check if left is not empty
+			left.printPostOrder(); // recursively print left
+		if (right != null) // check if right is not empty
+			right.printPostOrder(); // recursively print right
+		System.out.print(data + " "); // print current node
 	}
 }
 
