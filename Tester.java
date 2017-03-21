3898fd368a77ef00592e215a0f487693ffec7874
Ashwin Kamalakannan <ashwin.kamalakannan@uoit.net>
2017-03-21 12:58:34 -0400

    added all documentation

--- Tester.java
+++ Tester.java
@@ -1,63 +1,94 @@
+/**
+ * @authors:
+ * 		Darron Singh
+ * 		Jasindan Rasalingam
+ * 		Kaushal Patel
+ * 		Ashwin Kamalakannan
+ * 
+ * - Binary tree testing class
+ * - Contains methods:
+ * 		- Load(int[], String) constructs binary tree from given traversal and type O(n) time
+ * 		- printTree(Node) prints all traversals of a given tree in O(n) time
+ * 		- main(String[]) contains test cases and input 
+ */
+
 public class Tester {
-	
+
+	/*
+	 * Takes an integer array for the data points Takes a String containing the
+	 * type of traversal constructs tree with given information and returns it
+	 */
 	public static Node load(int[] input, String traversal) {
-		Node root = null;
-		int start = 0, finish = 0, dir = 0;
-		
-		if (traversal.equals("inorder")) {
-			root = new Node(input[0]);
-			start = 1; finish = input.length; dir = 1;
-		} else if (traversal.equals("preorder")) {
-			root = new Node(input[0]);
-			start = 1; finish = input.length; dir = 1;
-		} else if (traversal.equals("postorder")) {
-			root = new Node(input[input.length-1]);
-			start = input.length-1; finish = -1; dir = -1;
+		Node root = null; // root for tree starts null
+		int start = 0, finish = 0, dir = 0; // initialize variables
+
+		if (traversal.equals("preorder")) { // preorder given
+			root = new Node(input[0]); // start at index 0
+			start = 1;
+			finish = input.length;
+			dir = 1;
+		} else if (traversal.equals("inorder")) { // inorder given
+			root = new Node(input[0]); // start at index 0
+			start = 1;
+			finish = input.length;
+			dir = 1;
+		} else if (traversal.equals("postorder")) { // postorder given
+			root = new Node(input[input.length - 1]); // start at index end
+			start = input.length - 1;
+			finish = -1;
+			dir = -1;
 		}
-		
-		for (int i=start; i != finish; i += dir) {
-			if (input[i] < root.data) {
-				if (root.left == null)
-					root.left = new Node(input[i]);
-				else
-					root.left.insert(input[i]);
-			}
-			else if (input[i] > root.data) {
-				if (root.right == null)
-					root.right = new Node(input[i]);
-				else
-					root.right.insert(input[i]);
-			}
-			else 
-				root.data = input[i];
+
+		// loop through input array and load the tree
+		for (int i = start; i != finish; i += dir) {
+			if (input[i] < root.data) { // if input < root
+				if (root.left == null) // check if left is null
+					root.left = new Node(input[i]); // create left node
+				else // if it wasn't empty
+					root.left.insert(input[i]); // call recursive insert
+			} else if (input[i] > root.data) { // if input > root
+				if (root.right == null) // check if right is null
+					root.right = new Node(input[i]); // create right node
+				else // if it wasn't empty
+					root.right.insert(input[i]); // call recursive insert
+			} else // if neither
+				root.data = input[i]; // root data set equal to input
 		}
-		
-		return root;
+		return root; // return the tree at the end
 	}
-	
+
+	/*
+	 * Print all traversals for a given tree
+	 */
 	public static void printTree(Node root) {
 		System.out.print("preorder: ");
-		root.printInOrder();
+		root.printPreOrder(); // print preorder
 		System.out.println();
 		System.out.print("inorder: ");
-		root.printPreOrder();
+		root.printInOrder(); // print inorder
 		System.out.println();
 		System.out.print("postorder: ");
-		root.printPostOrder();
+		root.printPostOrder(); // print postorder
 		System.out.println("\n");
 	}
 	
+	/*
+	 * main method, sets up input and all test cases
+	 */
 	public static void main(String args[]) {
-		int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-		
+		int[] input = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }; // input array
+
+		// load tree with preorder traversal and print it
 		System.out.println("Pre-order given");
 		printTree(load(input, "preorder"));
-		
+
+		// load tree with inorder traversal and print it
 		System.out.println("In-order given");
 		printTree(load(input, "inorder"));
-		
+
+		// load tree with postorder traversal and print it
 		System.out.println("Post-order given");
 		printTree(load(input, "postorder"));
-		
+
 	}
 }
